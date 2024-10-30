ea883dabbffc7a2ca2476bf7faf339e1b3096b13
Ashish Gupta <ash75356@gmail.com>
2024-10-30 20:25:25 +0530

    Updated site

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ const connect  = async () => {
     }
 }
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.VITE_PROD_BASE_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
